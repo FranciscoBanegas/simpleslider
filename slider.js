45896390aaf18d50b12e519252b6c5638684e83a
FranciscoBanegas <fbanegas85@gmail.com>
2023-11-25 20:07:27 -0300

    Simple slider

---
+++ slider.js
@@ -0,0 +1,46 @@
+// let currentIndex = 0;
+// // selecion los elementos dentro del slider y obtengo la cantidad total
+// const totalSlides = document.querySelectorAll('.slide').length;
+// console.log(totalSlides);
+// function updateSlider() {
+//   // selecionamos los elementos dentro del slider
+//   const slider = document.querySelector('.slider');
+//   // transformamos el slider en una transicion
+//   slider.style.transform = `translateX(${-currentIndex * 100}%)`;
+// }
+
+// function nextSlide() {
+//   currentIndex = (currentIndex + 1) % totalSlides;
+//   updateSlider();
+// }
+
+// function prevSlide() {
+//   currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+//   updateSlider();
+// }
+
+// conexion
+console.log('todo correcto');
+let currentIndex = 0;
+// selecion los elementos dentro del slider y obtengo la cantidad total
+const totalSlides = document.querySelectorAll('.slide').length;
+console.log(totalSlides);
+function updateSlider() {
+  // selecionamos los elementos dentro del slider
+  const slider = document.querySelector('.slider');
+  // transformamos el slider en una transicion
+  slider.style.transform = `translateX(${-currentIndex * 100}%)`;
+}
+
+document.querySelector('.content-btn').addEventListener('click', (e) => {
+  const next = document.querySelector('.next-btn');
+  const prev = document.querySelector('.prev-btn');
+  console.log(e.target);
+  if(next && e.target === next){
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateSlider();
+  }else if(prev && e.target === prev){
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateSlider();
+  } 
+})
