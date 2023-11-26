3ee84462c3d745cde002c0631fe575080bee2186
FranciscoBanegas <fbanegas85@gmail.com>
2023-11-25 23:40:43 -0300

    Simplificado

--- slider.js
+++ slider.js
@@ -1,23 +1,28 @@
+// indice actual
 let currentIndex = 0;
-// selecion los elementos dentro del slider y obtengo la cantidad total
+//contador cantidad de elementos
 const totalSlides = document.querySelectorAll('.slide').length;
-console.log(totalSlides);
-function updateSlider() {
-  // selecionamos los elementos dentro del slider
-  const slider = document.querySelector('.slider');
-  // transformamos el slider en una transicion
-  slider.style.transform = `translateX(${-currentIndex * 100}%)`;
-}
 
 document.querySelector('.content-btn').addEventListener('click', (e) => {
+// selecionamos los elementos 
   const next = document.querySelector('.next-btn');
   const prev = document.querySelector('.prev-btn');
-  console.log(e.target);
+  const slider = document.querySelector('.slider'); 
+  /*
+  Cremos una condicion para saber cual elemento se presiono
+  ejemplo el usuario precione el boton next o prev
+  */
   if(next && e.target === next){
+   /*
+    Currentindex es cero y se ira 
+    Sumando en uno en uno y cuando el residuo llegue a 0 empezara de nuevo 
+    ejemplo: 0 + 1 = 1, 1 + 1 = 2, 2 + 1 = 3 % 3 = 0
+    */
     currentIndex = (currentIndex + 1) % totalSlides;
-    updateSlider();
-  }else if(prev && e.target === prev){
+    }else if(prev && e.target === prev){
+    //ejemplo: 3 - 1 = 2, 2 - 1 = 1, 1 - 1 = 0 % 3 = 0
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    updateSlider();
   } 
+  //Añadimos la propieda transform y los desplazamos a la derecha de 100%
+  slider.style.transform = `translateX(${-currentIndex * 100}%)`;
 })
